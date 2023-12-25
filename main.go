b20bdb036906379fb5859c52fdb2006c167e0273
Aldiyar <aldiyarmaratov0@gmail.com>
2023-12-25 21:09:29 +0600

    assigment 1

--- main.go
+++ main.go
@@ -6,12 +6,10 @@ import (
 	"net/http"
 )
 
-// RequestBody represents the structure of the incoming JSON data
 type RequestBody struct {
 	Message string `json:"message"`
 }
 
-// ResponseBody represents the structure of the outgoing JSON response
 type ResponseBody struct {
 	Status  string `json:"status"`
 	Message string `json:"message"`
@@ -24,13 +22,12 @@ func main() {
 }
 
 func handleRequest(w http.ResponseWriter, r *http.Request) {
-	// Only handle POST requests
+
 	if r.Method != http.MethodPost {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
 
-	// Decode JSON from the request body
 	var reqBody RequestBody
 	decoder := json.NewDecoder(r.Body)
 	err := decoder.Decode(&reqBody)
@@ -39,22 +36,18 @@ func handleRequest(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Check if the "message" field is present
 	if reqBody.Message == "" {
 		http.Error(w, "Invalid JSON message", http.StatusBadRequest)
 		return
 	}
 
-	// Print the message to the server console
 	fmt.Println("Received message:", reqBody.Message)
 
-	// Respond with success message
 	resBody := ResponseBody{
 		Status:  "success",
 		Message: "Data successfully received",
 	}
 
-	// Encode and send the JSON response
 	w.Header().Set("Content-Type", "application/json")
 	encoder := json.NewEncoder(w)
 	encoder.Encode(resBody)
